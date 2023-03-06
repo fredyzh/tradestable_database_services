210c07add34d1eeb347878ce28e949a0fdac52f7
Feng Zhang <fredyzh@gmail.com>
2023-03-06 14:47:42 -0500

    clean up

--- src/main/java/com/stabletrader/v2/databaseservices/services/StockService.java
+++ src/main/java/com/stabletrader/v2/databaseservices/services/StockService.java
@@ -3,7 +3,6 @@ package com.stabletrader.v2.databaseservices.services;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
